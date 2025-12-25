6bb6af8d4a49e5617488d7330d99e181e961e3ad
Maksym Shamrai <150514219+maksymshamrai@users.noreply.github.com>
2025-12-25 13:23:28 +0100

    Update App.jsx

--- hw1/src/App.jsx
+++ hw1/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const name = 'Maksym';
+  const favoriteSite = { name: "YouTube", url: "https://www.youtube.com/" };
+  
+  const a = 3;
+  const b = 4;
+  const sum = a + b;
+
+  const colors = ["Червоний", "Синій", "Зелений"];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{name}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img src={reactLogo} className="logo react" alt="React logo" /><br/>
+      <a href="{favoriteSite.url}">{favoriteSite.name}</a>
+      <p>{sum}</p>
+      <ul>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
     </>
   )
 }
